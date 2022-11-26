82245bed73688d06eed090e9ed3d474a764e1c3b
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-11-26 23:22:03 +0100

    Composant Menu qui permet de naviger à travers l'application web (sauf admin)

--- front/src/App.js
+++ front/src/App.js
@@ -6,18 +6,19 @@ import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
 import MonCompte from './components/MonCompte/MonCompte';
 import { Admin } from './components/Admin/Admin';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <>
+      <Menu />
+      
       <Routes>
         <Route path="SignUp" element={<SignUp />} />
         <Route path="SignIn" element={<SignIn />} />
         <Route path="MonCompte/*" element={<MonCompte />} />
         <Route path="Admin/*" element={<Admin />} />
       </Routes>
-
-      <NavLink to="SignIn">Connexion</NavLink>
     </>
   )
 }
---
+++ front/src/components/Menu/Menu.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import "./Menu.scss";
+
+function Menu() {
+    return (
+        <div id="Menu">
+            <NavLink to="/" className="logo">
+                <span className="red">car</span>elec
+            </NavLink>
+            <NavLink to="/NotreGarage">Notre garage</NavLink>
+            <NavLink to="/Aides">Besoin d'aides</NavLink>
+            <NavLink to="/MonCompte" className="accountLogo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path id="Tracé_1" data-name="Tracé 1" d="M14,2A12,12,0,1,0,26,14,12,12,0,0,0,14,2Zm0,3.6a3.6,3.6,0,1,1-3.6,3.6A3.6,3.6,0,0,1,14,5.6Zm0,17.04a8.641,8.641,0,0,1-7.2-3.864c.036-2.388,4.8-3.7,7.2-3.7s7.164,1.308,7.2,3.7A8.641,8.641,0,0,1,14,22.64Z" transform="translate(-2 -2)" fill="#fff" />
+                </svg>
+            </NavLink>
+        </div>
+    )
+}
+
+export default Menu;
