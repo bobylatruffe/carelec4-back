aa1daefb7bdc3354884df2f91524fb64e2a6aa0c
Stykame <marc.lozachmeur@hotmail.fr>
2022-11-28 19:32:18 +0100

    composants CONTACT et FOOTER

---
+++ front/src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import "./Footer.css"
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <nav className='footer'>
+        <div className='footer-container'>
+          <ul>
+            <li><a href='/' className='logo'><span className='red'>Car</span>Elec</a></li>
+            <br></br>
+            <div id='col'>
+              <p>
+                <li><a href='/notre-garage'>Qui sommes-nous ?</a></li><br></br>
+                <li><a href='/contact'>Contactez-nous</a></li>
+              </p>
+              <p>
+                <br></br>
+                <li><a href='#'>Mentions légales</a></li><br></br>
+                <li><a href='#'>Conditions générales</a></li>
+              </p>
+              <p>
+                <li><a href='/'>Nous sommes ouverts du lundi au vendredi <br></br>de 9h à 12h et de 13h à 17h30.<br></br>Le samedi de 9h à 12h.</a></li>
+              </p>
+            </div>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Footer;
